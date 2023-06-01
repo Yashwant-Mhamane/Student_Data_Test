71e1b874870d62b4e55ed837cbc2c617c8755adf
yashwantmhamane <yashwantmhamane@gmail.com>
2023-06-01 12:10:08 +0530

    Fields added and annotation added to it @Entity & @Id

--- src/main/java/com/test/atdev/student_data/domain/Student.java
+++ src/main/java/com/test/atdev/student_data/domain/Student.java
@@ -1,4 +1,15 @@
 package com.test.atdev.student_data.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
+    private int studentId;
+    private String studentName;
+    private String studentClass;
+    private String studentMailId;
+    private int studentMarks;
+
 }
