331191e5c1ddbbcd7ea9267e63104731872d1418
yashwantmhamane <yashwantmhamane@gmail.com>
2023-06-01 12:19:53 +0530

    CURD operation method created

--- src/main/java/com/test/atdev/student_data/service/StudentService.java
+++ src/main/java/com/test/atdev/student_data/service/StudentService.java
@@ -1,4 +1,16 @@
 package com.test.atdev.student_data.service;
 
+import com.test.atdev.student_data.domain.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student addStudent(Student student);
+
+    boolean updateStudentDetails(Student updatedStudent, Integer studentId) throws Exception;
+
+    boolean deleteStudent(Integer studentId);
+
 }
