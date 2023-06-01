6ae655d8024a2dcb46a901e9f04f48d1c1ff555c
yashwantmhamane <yashwantmhamane@gmail.com>
2023-06-01 13:05:27 +0530

    StudentService injected by constructor Autowiring

--- src/main/java/com/test/atdev/student_data/controller/StudentController.java
+++ src/main/java/com/test/atdev/student_data/controller/StudentController.java
@@ -1,4 +1,13 @@
 package com.test.atdev.student_data.controller;
 
+import com.test.atdev.student_data.service.StudentService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+  private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 }
