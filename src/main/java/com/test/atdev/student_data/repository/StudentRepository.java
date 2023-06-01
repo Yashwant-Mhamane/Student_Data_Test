9dcaf32f44161594e15a95bfe7cbb9342274e6e6
yashwantmhamane <yashwantmhamane@gmail.com>
2023-06-01 12:16:04 +0530

    @Repository annotation added and extends CrudRepository

--- src/main/java/com/test/atdev/student_data/repository/StudentRepository.java
+++ src/main/java/com/test/atdev/student_data/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.test.atdev.student_data.repository;
 
-public interface StudentRepository {
+import com.test.atdev.student_data.domain.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student,Integer> {
+
 }
