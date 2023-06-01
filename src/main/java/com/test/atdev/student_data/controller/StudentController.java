c9a3bc679ce9dc13c155d9006791caea82f6cefa
yashwantmhamane <yashwantmhamane@gmail.com>
2023-06-01 13:12:44 +0530

    getAllStudent Getmapping method added

--- src/main/java/com/test/atdev/student_data/controller/StudentController.java
+++ src/main/java/com/test/atdev/student_data/controller/StudentController.java
@@ -1,7 +1,14 @@
 package com.test.atdev.student_data.controller;
 
+import com.test.atdev.student_data.domain.Student;
+import com.test.atdev.student_data.exception.StudentNotFoundException;
 import com.test.atdev.student_data.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -10,4 +17,15 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
+    @GetMapping("/students/get")
+    public ResponseEntity<?> getAllStudents() throws StudentNotFoundException {
+        List<Student> studentList = studentService.getAllStudents();
+        if (studentList.isEmpty()) {
+            return new ResponseEntity<>("Empty Database.",HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(studentList, HttpStatus.FOUND);
+        }
+    }
+
 }
